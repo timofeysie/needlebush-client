8bd7dab18f2f82e850df70d65e1d989c455ebf4b
Timothy Curchod <timofeyc@hotmail.com>
2016-10-15 22:47:05 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Client
+
+## Local development
+
+Running the app locally, replace
+```
+https://git.heroku.com/needlebush.git
+```
+With http://localhost:8080
+```
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { Storage } from '@ionic/storage';
+import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login-page/login-page';
+import { SignupPage } from '../pages/signup-page/signup-page';
+import { Todos } from '../providers/todos';
+import { Auth } from '../providers/auth';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    LoginPage,
+    SignupPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    LoginPage,
+    SignupPage
+  ],
+  providers: [Storage, Todos, Auth]
+})
+export class AppModule {}
